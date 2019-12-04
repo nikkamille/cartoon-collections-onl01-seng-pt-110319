f25c8f933df412b80b5ed8d8db43d8c481ff7f18
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-04 02:34:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,7 @@ end
 def find_the_cheese(cheese) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   maybe_cheese = ["mango", "cheddar", "shoe"]
-  cheese.find do |type| cheese.include?(maybe_cheese) }
+  cheese.find do |type| 
+    cheese.include?(maybe_cheese) 
+  end
 end
