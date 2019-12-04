35220e085de853185f41b6c92f879ad0357b8c20
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-04 01:54:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves) # code an argument here
 end
 
 def summon_captain_planet(planeteer_calls) # code an argument here
-  planeteer_calls.map!
+  planeteer_calls.map! {
 end
 
 def long_planeteer_calls# code an argument here
