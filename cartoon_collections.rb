87a902eeca9adf1e156ff21caeb1aa9132022873
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-04 02:09:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls) # code an argument here
   planeteer_calls.map! { |calls| calls.capitalize + "!" }
 end
 
-def long_planeteer_calls # code an argument here
-  # Your code here
+def long_planeteer_calls(calls) # code an argument here
+  
 end
 
 def find_the_cheese# code an argument here
