3c1f28631f734f7059db36b1222a9d8871a50d07
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-04 02:33:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,4 @@ def find_the_cheese(cheese) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   maybe_cheese = ["mango", "cheddar", "shoe"]
   cheese.find { |type| cheese.include?(maybe_cheese) }
-  end
 end
