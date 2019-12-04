01d9051d46901150344ff405ce8cffe90b7b2def
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-04 01:45:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarves) # code an argument here
-  dwarves.each_with_index
+  dwarves.each_with_index do |name|
+    puts ""
 end
 
 def summon_captain_planet# code an argument here
